66761eb4df52e31840ab9d25c3a5798f1d7eb218
Ben Newman <ben@meteor.com>
2015-05-07 11:18:55 -0700

    Eliminate Future from tools/console.js.

--- tools/console.js
+++ tools/console.js
@@ -57,7 +57,6 @@
 /// support, that is mostly handled through buildmessage.js.
 var _ = require('underscore');
 var Fiber = require('fibers');
-var Future = require('fibers/future');
 var readline = require('readline');
 var util = require('util');
 var buildmessage = require('./buildmessage.js');
@@ -1253,8 +1252,6 @@ _.extend(Console.prototype, {
 Console.prototype.readLine = function (options) {
   var self = this;
 
-  var fut = new Future();
-
   options = _.extend({
     echo: true,
     stream: self._stream
@@ -1291,15 +1288,15 @@ Console.prototype.readLine = function (options) {
     rl.prompt();
   }
 
-  rl.on('line', function (line) {
-    rl.close();
-    if (! options.echo)
-      options.stream.write("\n");
-    self._setProgressDisplay(previousProgressDisplay);
-    fut['return'](line);
-  });
-
-  return fut.wait();
+  return new Promise(function (resolve) {
+    rl.on('line', function (line) {
+      rl.close();
+      if (! options.echo)
+        options.stream.write("\n");
+      self._setProgressDisplay(previousProgressDisplay);
+      resolve(line);
+    });
+  }).await();
 };
 
 
