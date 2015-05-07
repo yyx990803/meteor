adfebca7c541df6342baa6bf34b1a83ec18c7fcc
Ben Newman <ben@meteor.com>
2015-05-07 15:02:41 -0700

    Eliminate Future from tools/main.js.

--- tools/main.js
+++ tools/main.js
@@ -5,7 +5,6 @@ if (showRequireProfile)
 var assert = require("assert");
 var _ = require('underscore');
 var Fiber = require('fibers');
-var Future = require('fibers/future');
 var Console = require('./console.js').Console;
 var files = require('./files.js');
 var warehouse = require('./warehouse.js');
@@ -476,14 +475,13 @@ var springboard = function (rel, options) {
   }
 
   if (process.platform === 'win32') {
-    var ret = new Future();
-    var child = require("child_process").spawn(
-      files.convertToOSPath(executable + ".bat"), newArgv,
-      { env: process.env, stdio: 'inherit' });
-    child.on('exit', function (code) {
-      ret.return(code);
-    });
-    process.exit(ret.wait());
+    process.exit(new Promise(function (resolve) {
+      var batPath = files.convertToOSPath(executable + ".bat");
+      var child = require("child_process").spawn(batPath, newArgv, {
+        env: process.env,
+        stdio: 'inherit'
+      }).on('exit', resolve);
+    }).await());
   }
 
   // Now exec; we're not coming back.
