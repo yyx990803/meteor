5c0b7b806f0494823e8d8d12f732dc6d39628b3b
Sashko Stubailo <sashko@stubailo.com>
2015-07-30 20:40:19 -0700

    Give mongo a special place

--- packages/meteor-platform/package.js
+++ packages/meteor-platform/package.js
@@ -11,25 +11,10 @@ Package.onUse(function(api) {
   // If you want, you can "meteor remove meteor-platform" and add some of
   // these back in individually. We haven't tested every subset, though :)
   api.imply([
-    // The normal "every package uses 'meteor'" rule only applies to packages
-    // built from a package source directory, so we make sure apps get it too.
-    // Meteor.isServer! The CSS extension handler! And so much more!
-    'meteor',
-    // A standard Meteor app is a web app. (Without this, there will be no
-    // 'main' function unless you define one yourself.)
-    'webapp',
     // It's Log! It's better than bad, it's good!
     'logging',
-    // Tracker.autorun and friends. What's Meteor without reactivity?
-    'tracker',
-    'deps', // XXX COMPAT WITH PACKAGES BUILT FOR 0.9.0.
     // The easiest way to get a little reactivity into your app.
     'session',
-    // DDP: Meteor's client/server protocol.
-    'ddp',
-    'livedata', // XXX COMPAT WITH PACKAGES BUILT FOR 0.9.0.
-    // You want to keep your data somewhere? How about MongoDB?
-    'mongo',
     // Blaze: Reactive DOM!
     'blaze',
     'ui', // XXX COMPAT WITH PACKAGES BUILT FOR 0.9.0.
@@ -39,10 +24,6 @@ Package.onUse(function(api) {
     'templating',
     // Easy type assertions? check.
     'check',
-    // _.isUseful(true)
-    'underscore',
-    // $(".usefulToo")
-    'jquery',
     // Life isn't always predictable.
     'random',
     // People like being able to clone objects.
@@ -58,5 +39,7 @@ Package.onUse(function(api) {
     'autoupdate'
   ], ['client', 'server']);
 
+  api.imply("meteor-base");
+  api.imply("mongo");
   api.imply("mobile-experience");
 });
